e5913b657764c3a6307c5b5f7092709b51d6b722
s51517765 <nakasha348@gmail.com>
2021-11-23 18:08:58 +0900

    リファクタリング

---
+++ ブロックくずし/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace ブロックくずし
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonPlayer = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelBall = new System.Windows.Forms.Label();
+            this.buttonSpeedUp = new System.Windows.Forms.Button();
+            this.buttonSpeedDown = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonPlayer
+            // 
+            this.buttonPlayer.BackColor = System.Drawing.Color.Blue;
+            this.buttonPlayer.Location = new System.Drawing.Point(418, 423);
+            this.buttonPlayer.Name = "buttonPlayer";
+            this.buttonPlayer.Size = new System.Drawing.Size(86, 23);
+            this.buttonPlayer.TabIndex = 0;
+            this.buttonPlayer.UseVisualStyleBackColor = false;
+            this.buttonPlayer.Click += new System.EventHandler(this.buttonPlayer_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelBall
+            // 
+            this.labelBall.AutoSize = true;
+            this.labelBall.Location = new System.Drawing.Point(447, 270);
+            this.labelBall.Name = "labelBall";
+            this.labelBall.Size = new System.Drawing.Size(22, 15);
+            this.labelBall.TabIndex = 1;
+            this.labelBall.Text = "●";
+            // 
+            // buttonSpeedUp
+            // 
+            this.buttonSpeedUp.Location = new System.Drawing.Point(14, 176);
+            this.buttonSpeedUp.Name = "buttonSpeedUp";
+            this.buttonSpeedUp.Size = new System.Drawing.Size(49, 23);
+            this.buttonSpeedUp.TabIndex = 2;
+            this.buttonSpeedUp.Text = "+";
+            this.buttonSpeedUp.UseVisualStyleBackColor = true;
+            this.buttonSpeedUp.Click += new System.EventHandler(this.buttonSpeedUp_Click);
+            // 
+            // buttonSpeedDown
+            // 
+            this.buttonSpeedDown.Location = new System.Drawing.Point(14, 205);
+            this.buttonSpeedDown.Name = "buttonSpeedDown";
+            this.buttonSpeedDown.Size = new System.Drawing.Size(49, 23);
+            this.buttonSpeedDown.TabIndex = 3;
+            this.buttonSpeedDown.Text = "-";
+            this.buttonSpeedDown.UseVisualStyleBackColor = true;
+            this.buttonSpeedDown.Click += new System.EventHandler(this.buttonSpeedDown_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(14, 127);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(89, 29);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 13F);
+            this.label1.Location = new System.Drawing.Point(234, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Debug";
+            this.label1.Visible = false;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(14, 270);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(89, 29);
+            this.buttonReset.TabIndex = 6;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, -1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 30);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(240, -1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 30);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(320, -1);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 30);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(400, -1);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 30);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(480, -1);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 30);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(560, -1);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 30);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(640, -1);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(80, 30);
+            this.button8.TabIndex = 14;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(720, -1);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(80, 30);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(800, -1);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(80, 30);
+            this.button10.TabIndex = 16;
+            this.button10.Text = "10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(0, -1);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(80, 30);
+            this.button0.TabIndex = 17;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 484);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonSpeedDown);
+            this.Controls.Add(this.buttonSpeedUp);
+            this.Controls.Add(this.labelBall);
+            this.Controls.Add(this.buttonPlayer);
+            this.Name = "Form1";
+            this.Text = "ブロックくずし Ver.2019-05-09";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPlayer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelBall;
+        private System.Windows.Forms.Button buttonSpeedUp;
+        private System.Windows.Forms.Button buttonSpeedDown;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button0;
+    }
+}
+
---
+++ ブロックくずし/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ブロックくずし
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int playerSize_X = 75;
+        const int playerPosition_Y = 430;
+        double ball_Speed = 8;
+
+        double ball_Direction = Math.PI / 4;
+        int Form1_x = 0;
+        int Cursol_X = 0;
+        bool hitting = false; //打ち返し中
+
+        bool[] Block_flag = { true, true, true, true, true, true, true, true, true, true, true };  //ブロックが残っているか
+
+        private void buttonPlayer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Form1_x = this.Left; //Form1の位置
+            Cursol_X = MousePosition.X - Form1_x - playerSize_X / 2;
+            buttonPlayer.Left = Cursol_X;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonPlayer.Location = new Point(0, playerPosition_Y);
+            labelBall.Location = new Point(50, 50);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //ボールの進行角度を 0～2*PIに正規化
+            if (ball_Direction < 0) ball_Direction += Math.PI * 2;
+            else if (ball_Direction > Math.PI * 2) ball_Direction -= Math.PI * 2;
+
+            int ball_X = labelBall.Left;　//ボールの位置を取得
+            int ball_Y = labelBall.Top;
+
+            //横壁との反射
+            if (ball_X < 0 || this.Width - 30 < ball_X)
+            {
+                ball_Direction = 2 * Math.PI - ball_Direction;
+            }
+
+            //縦壁との反射
+            if (ball_Y < 0 || this.Height - 60 < ball_Y)
+            {
+                ball_Direction = Math.PI - ball_Direction;
+            }
+
+            //ブロック当たり判定
+            if (ball_Y < 30) attack(ball_X, ball_Y);
+
+            //パドル当たり判定
+            if (Math.Abs(playerPosition_Y - ball_Y) < ball_Speed && hitting == false) //高さ方向を限定&&打ち返し中でない
+            {
+                int center_distance = Cursol_X + playerSize_X / 2 - ball_X;
+                if (Math.Abs(center_distance) <= 5) //中心
+                {
+                    ball_Direction = Math.PI - ball_Direction;
+                }
+                else if (5 < center_distance && center_distance < 25) //端っこに当たったら反射角度を変える
+                {
+                    if (ball_Direction > Math.PI * 0.125 && Math.PI * 1.875 > ball_Direction) ball_Direction += Math.PI * 0.95;
+                }
+                else if (25 <= center_distance && center_distance < 50) //端っこに当たったら反射角度を変える
+                {
+                    if (ball_Direction > Math.PI * 0.125 && Math.PI * 1.875 > ball_Direction) ball_Direction += Math.PI * 1.03;
+                }
+
+                hitting = true;　//打ち返し中フラグ
+            }
+            else if (Math.Abs(playerPosition_Y - ball_Y) > ball_Speed && hitting == true)　//打ち返し中を抜けたら
+            {
+                hitting = false;
+            }
+            labelBall.Left += (int)(ball_Speed * Math.Sin(ball_Direction));
+            labelBall.Top += (int)(ball_Speed * Math.Cos(ball_Direction));
+            //   label1.Text = ((int)(ball_Speed * Math.Sin(ball_Direction))).ToString() + " , " + ((int)(ball_Speed * Math.Cos(ball_Direction))).ToString() + " , " + ball_Direction.ToString();
+
+            Application.DoEvents(); //画面更新その他をすべて処理
+        }
+        private void game_end()
+        {
+            timer1.Stop();
+            MessageBox.Show("Game Clear!");
+            buttonStart.Enabled = false;
+        }
+        private void attack(int ball_x, int ball_y)
+        {
+            int count = 0;
+
+            for (int i = 0; i < Block_flag.Length; i++)
+            {
+                if (Block_flag[i]) count++;
+            }
+
+            int block_no = ball_x / 80;
+
+            if (ball_y < 30)
+            {
+                if (Block_flag[block_no])
+                {
+                    Block_flag[block_no] = false;
+
+                    switch (block_no)
+                    {
+                        case 0:
+                            button0.Visible = false;
+                            break;
+                        case 1:
+                            button1.Visible = false;
+                            break;
+                        case 2:
+                            button2.Visible = false;
+                            break;
+                        case 3:
+                            button3.Visible = false;
+                            break;
+                        case 4:
+                            button4.Visible = false;
+                            break;
+                        case 5:
+                            button5.Visible = false;
+                            break;
+                        case 6:
+                            button6.Visible = false;
+                            break;
+                        case 7:
+                            button7.Visible = false;
+                            break;
+                        case 8:
+                            button8.Visible = false;
+                            break;
+                        case 9:
+                            button9.Visible = false;
+                            break;
+                        case 10:
+                            button10.Visible = false;
+                            break;
+                    }
+                    if (count == 1) //残り一個でブロックを消したとき
+                    {
+                        game_end();
+                    }
+
+                    ball_Direction = Math.PI - ball_Direction;
+
+                }
+            }
+        }
+
+
+        private void buttonSpeedUp_Click(object sender, EventArgs e)
+        {
+            ball_Speed *= 1.2;
+        }
+
+        private void buttonSpeedDown_Click(object sender, EventArgs e)
+        {
+            ball_Speed /= 1.2;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                timer1.Start();
+                buttonStart.Text = "Stop";
+                buttonStart.ForeColor = Color.Red;
+            }
+            else
+            {
+                timer1.Stop();
+                buttonStart.Text = "Start";
+                buttonStart.ForeColor = Color.Black;
+            }
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            labelBall.Left = 50;
+            labelBall.Top = 50;
+            ball_Direction = Math.PI / 4;
+            button0.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+            button7.Visible = true;
+            button8.Visible = true;
+            button9.Visible = true;
+            button10.Visible = true;
+
+            buttonStart.Enabled = true;
+
+            for (int i = 0; i < Block_flag.Length; i++) Block_flag[i] = true;
+        }
+    }
+}
